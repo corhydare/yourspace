f52906ae5a0848c6cca128b46422f8b85ac0221b
Unknown <danila.vpopov@gmail.com>
2022-05-03 19:02:41 -0700

    server is changing

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const db = require("./config/connection");
+const routes = require("./routes");
 
+const cwd = process.cwd();
 const app = express();
 const PORT = process.env.PORT || 3001;
-app.use(require("./routes"));
+app.use(routes);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 db.once("open", () => {
   app.listen(PORT, () => {
-    console.log(`API server for ${activity} running on port ${PORT}!`);
+    console.log(`API server running on port ${PORT}!`);
   });
 });
